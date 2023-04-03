6b35806b091eba37ec6a980ae8407371fa9a632e
Lucas Scharenbroich <lscharen@prowestgis.com>
2023-04-03 10:54:02 -0500

    Generalize build script for PRD environment

--- build-push-deploy.sh
+++ build-push-deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# AWS_ACCOUNT_ID=998343784597 for SIT
+# AWS_ACCOUNT_ID=824888059401 for STG and PRD
 set -e
 
-docker build --platform linux/amd64 -t sit-stac-fastapi . -f docker/Dockerfile
+docker build --platform linux/amd64 -t ${STAGE}-stac-fastapi . -f docker/Dockerfile
 
-docker tag sit-stac-fastapi 998343784597.dkr.ecr.us-east-1.amazonaws.com/sit-stac-fastapi:latest
+docker tag ${STAGE}-stac-fastapi ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${STAGE}-stac-fastapi:latest
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 998343784597.dkr.ecr.us-east-1.amazonaws.com 
+aws ecr get-login-password --profile default --region us-east-1 | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com 
 
-docker push 998343784597.dkr.ecr.us-east-1.amazonaws.com/sit-stac-fastapi:latest
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/${STAGE}-stac-fastapi:latest
 
-aws ecs update-service --force-new-deployment --cluster sit-vpc-ECSCluster-OSV5bONITro4 --service sit-stacapi
+#aws ecs update-service --force-new-deployment --cluster sit-vpc-ECSCluster-OSV5bONITro4 --service ${STAGE}-stacapi
